317587760f8c052d85ca72f3d54c0cbf7b78070c
Diego <diego.planichek@gmail.com>
2025-10-03 22:10:35 -0300

    feat: create product register page e component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
+import Register from "./Register";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
