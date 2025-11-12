21fd206ff9c5dc3d1688ac0e5ef7749e4de6a64d
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2025-11-12 19:59:14 -0300

    feat: login

--- src/components/SidebarComponent/SidebarComponent.js
+++ src/components/SidebarComponent/SidebarComponent.js
@@ -1,3 +1,4 @@
+// SidebarComponent.jsx
 import { Menu } from 'primereact/menu';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -6,7 +7,7 @@ import './sidebar.css';
 
 import Cookies from 'js-cookie';
 
-export default function SidebarFixed({ isAdmin = false }) {
+export default function SidebarFixed({ isAdmin = false, kc }) {
     const cadastroItems = [
         { label: 'Clientes', icon: 'pi pi-user', command: () => console.log('Clientes clicked') },
         { label: 'Pedidos', icon: 'pi pi-shopping-cart', command: () => console.log('Pedidos clicked') },
@@ -20,17 +21,21 @@ export default function SidebarFixed({ isAdmin = false }) {
         { label: 'Aprovação de Pedidos', icon: 'pi pi-check', command: () => console.log('Aprovação clicked') },
     ];
 
-    function logout() {
+    const logout = () => {
         Cookies.remove("JWT_TOKEN");
-
-        window.location.href = '/';
-    }
+        if (kc) {
+            kc.logout({ redirectUri: "http://localhost:3000/" });
+        } else {
+            console.warn('Keycloak não encontrado, redirecionando manualmente...');
+            window.location.href = '/login';
+        }
+    };
 
     return (
         <div className="layout-container">
             <div className="layout-body">
                 <div className="sidebar">
-                    <div className="sidebar-header" onClick={() => logout()}>
+                    <div className="sidebar-header" onClick={logout}>
                         <i className="pi pi-arrow-left"></i>
                         <span>Sair</span>
                     </div>
